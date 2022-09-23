c0a55c7977d70d6e52fd6383b649ed984d0c6d93
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-09-23 17:21:42 +0300

    Add files via upload

---
+++ EscapeChars/EscapeChars/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("=> Escape Characters:\a");
+string strWithTabs = "Model\tColor\tSpeed\tPet Name \a ";
+Console.WriteLine(strWithTabs);
+
+Console.WriteLine("Everyone loves \"Hello World\"\a");
+Console.WriteLine("C:\\MyApp\\bin\\Debug\a");
+
+Console.WriteLine("All finished. \n\n\n\a ");
+Console.WriteLine();
