d91acf74fb6b0d980da7548bbab766057468a373
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-10-10 21:54:49 +0300

    SwitchLanguage
    repeated the switch theme

---
+++ SwitchRecall/SwitchRecall/Program.cs
@@ -0,0 +1,23 @@
+﻿static void SwitchExample()
+{
+    Console.WriteLine("1 [C#], 2 [VB]");
+    Console.WriteLine("Please pick you language preference: ");
+
+    string langChoice = Console.ReadLine();
+    int n = int.Parse(langChoice);
+
+    switch (n)
+    {
+        case 1:
+            Console.WriteLine("Good choice, C# is a fine language.");
+            break;
+        case 2:
+            Console.WriteLine("VB: OOP, multithreading, and more!");
+            break;
+        default:
+            Console.WriteLine("Well...good luck with that!");
+            break;
+    }
+}
+
+SwitchExample();
