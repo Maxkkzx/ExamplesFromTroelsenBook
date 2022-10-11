054ab0df941fddf67616fc3d4514887191f23fbd
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-10-12 00:00:52 +0300

    SwitchRecall
    I repeated this topic

---
+++ SwitchRecall/SwitchRecall/Program.cs
@@ -0,0 +1,50 @@
+﻿static void SwitchOnEnumExample()
+{
+    Console.Write("Enter your favorite day of the week: ");
+    DayOfWeek favDay;
+    try
+    {
+        favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Bad input!");
+        return;
+    }
+
+    switch (favDay)
+    {
+        case DayOfWeek.Sunday:
+            Console.WriteLine("Football!");
+            break;
+
+        case DayOfWeek.Monday:
+            Console.WriteLine("Another day, another dollar.");
+            break;
+
+        case DayOfWeek.Tuesday:
+            Console.WriteLine("At least it is not Monaday.");
+            break;
+
+        case DayOfWeek.Wednesday:
+            Console.WriteLine("A fine day.");
+            break;
+
+        case DayOfWeek.Thursday:
+            Console.WriteLine("Almost Friday...");
+            break;
+
+        case DayOfWeek.Friday:
+            Console.WriteLine("Yes, Friday rules!");
+            break;
+
+        case DayOfWeek.Saturday:
+            Console.WriteLine("Great day indeed.");
+            break;
+
+    }
+}
+
+
+SwitchOnEnumExample();
+Console.WriteLine();
