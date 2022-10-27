f9f201de6a8b2a9352fa2b6551e78e5984ac95a3
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-10-28 00:58:53 +0300

    ThisChain
    Building a chain of constructor calls using this.

---
+++ ThisChain/ThisChain/Motorcycle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisChain
+{
+    public class Motorcycle
+    {
+        public int driverIntensity;
+        public string driverName;
+
+        public Motorcycle() { }
+        public Motorcycle(int intensity)
+            :this(intensity, "") { }
+        public Motorcycle(string name)
+            : this(0, name) { }
+        public Motorcycle(int intensity, string name)
+        {
+            if(intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverIntensity = intensity;
+            driverName = name;
+        }
+
+        public void ShowMoto()
+        {
+            Console.Write(driverIntensity + " " + driverName);
+        }
+    }
+}
---
+++ ThisChain/ThisChain/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ThisChain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle motorcycle = new Motorcycle(20, "Bob");
+
+            motorcycle.ShowMoto();
+        }
+    }
+}
