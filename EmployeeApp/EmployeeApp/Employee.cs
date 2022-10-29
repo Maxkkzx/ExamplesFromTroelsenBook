2b578edc0d4a2dde8a9935a6f63da92552b250cd
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-10-30 02:26:25 +0300

    EmployeeApp
    The project that the employee describes

---
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSSN = ""  ;
+
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+            :this(name, 0, id, pay) { }
+
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;  
+            ID = id;    
+            Pay = pay;  
+        }
+        
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Ошибка! Длина имени превышает 15 символов!");
+                else
+                    empName= value;
+            }
+        }
+
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get { return empSSN; }
+        }
+
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Name: {empName}");
+            Console.WriteLine($"ID: {empID}");
+            Console.WriteLine($"Pay: {currPay}");
+            Console.WriteLine($"Age: {Age}");
+        }
+    }
+}
---
+++ EmployeeApp/EmployeeApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeApp;
+
+namespace EmployeeApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Marvin", 36, 456, 30000);
+
+            emp.DisplayStats();
+        }
+    }
+}
