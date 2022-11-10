53be3dd10464f66f688dc2571d1039ba2309743b
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-11-11 00:44:30 +0300

    InterfaceNameClash
    Explicit implementation of interfaces

---
+++ InterfaceNameClash/InterfaceNameClash/Class/Octagon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceNameClash.Interface;
+
+namespace InterfaceNameClash.Class
+{
+    class Octagon : IDrawToForm, IDrawToPrinter, IDrawToMemory
+    {
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to printer");
+        }
+    }
+}
---
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceNameClash.Interface;
+using InterfaceNameClash.Class;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Octagon oct = new Octagon();
+
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            ((IDrawToPrinter)oct).Draw();
+
+            if (oct is IDrawToMemory dtm)
+                dtm.Draw();
+        }
+    }
+}
