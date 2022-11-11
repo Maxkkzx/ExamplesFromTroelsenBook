e3b4c0de7245f71826e5e7cde4c1917773314a71
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-11-11 14:24:19 +0300

    Delete EscapeChars directory

--- EscapeChars/EscapeChars/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("=> Escape Characters:\a");
-string strWithTabs = "Model\tColor\tSpeed\tPet Name \a ";
-Console.WriteLine(strWithTabs);
-
-Console.WriteLine("Everyone loves \"Hello World\"\a");
-Console.WriteLine("C:\\MyApp\\bin\\Debug\a");
-
-Console.WriteLine("All finished. \n\n\n\a ");
-Console.WriteLine();
