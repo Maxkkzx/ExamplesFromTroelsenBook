9a085a4fe923f8d2e4e8fb50e3912227a76cf625
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-12-01 21:39:02 +0300

    CloneablePoint
    In this project I implemented the interface ICloneable

---
+++ CloneablePoint/CloneablePoint/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cloned p3 and stored new Point in p4");
+
+            Point p3 = new Point(100, 100, "Jane");
+            Point p4 = (Point)p3.Clone();
+
+            Console.WriteLine("Before modification:");
+            Console.WriteLine("p3: {0}", p3);
+            Console.WriteLine("p4: {0}", p4);
+
+            p4.desc.PetName = "My new Point";
+            p4.X = 9;
+
+            Console.WriteLine("\nChanged p4.desc.petName and p4.X");
+            Console.WriteLine("After modification:");
+            Console.WriteLine("p3: {0}", p3);
+            Console.WriteLine("p4: {0}", p4);
+        }
+    }
+
+    class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
+
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos;
+            Y = yPos;
+            desc.PetName = petName;
+        }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+
+        public Point() { }
+
+        public override string ToString() => $"X = {X}; Y = {Y}; Name = {desc.PetName};\nID = {desc.PointID}\n";
+        public object Clone()
+        {
+            Point newPoint = (Point)this.MemberwiseClone();
+
+            PointDescription currentDesc = new PointDescription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }
+    }
+
+    public class PointDescription
+    {
+        public string PetName { get; set; }
+        public Guid PointID { get; set; }
+
+        public PointDescription()
+        {
+            PetName = "No-name";
+            PointID = Guid.NewGuid();
+        }
+    }
+}
