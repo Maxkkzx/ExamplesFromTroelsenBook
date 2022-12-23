a81fda36d7b6775693eeb79b366c27adef482e73
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2022-12-24 00:22:00 +0300

    FunWithGenericCollections
    Generic Collections

---
+++ FunWithGenericCollections/FunWithGenericCollections/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericCollections
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }    
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}";
+        }
+    }
+}
---
+++ FunWithGenericCollections/FunWithGenericCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿using FunWithGenericCollections;
+
+static void UseGenericList()
+{
+    List<Person> people = new List<Person>()
+    {
+        new Person{FirstName = "Homer", LastName = "Simpson", Age = 47},
+        new Person{FirstName = "Marge", LastName = "Simpson", Age= 45},
+        new Person{FirstName = "Lisa", LastName = "Simpson", Age = 9},
+        new Person{FirstName = "Bart", LastName = "Simpson", Age = 8}
+    };
+
+    Console.WriteLine($"Items in list: {people.Count}");
+
+    foreach (Person p in people)
+        Console.WriteLine(p);
+
+    Console.WriteLine("\n ->Inserting new person.");
+
+    people.Insert(2,
+        new Person { FirstName = "Maggie", LastName = "Simpson", Age = 2 });
+    Console.WriteLine($"Items in list: {people.Count}");
+
+    Person[] arrrayOfPeople = people.ToArray();
+    foreach (Person p in arrrayOfPeople)
+        Console.WriteLine("First Names: {0}", p.FirstName);
+}
+UseGenericList();
