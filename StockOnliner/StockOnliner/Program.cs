edaddc68ed8eea2fb49de6f6f8d7134fa3130343
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-13 23:42:02 +0300

    StockOnliner
    Example to delegate

---
+++ StockOnliner/StockOnliner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockOnliner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
+            stockExchangeMonitor.Start();   
+        }
+
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine($"New price: {price}");
+        }
+    }
+}
