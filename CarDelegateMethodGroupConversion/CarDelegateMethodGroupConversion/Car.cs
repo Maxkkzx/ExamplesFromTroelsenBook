7adc4e861c8684df99f86ec49e68d4c58572f43b
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-14 23:56:05 +0300

    CarDelegateMethodGroupConversion
    Delegate with group conversion

---
+++ CarDelegateMethodGroupConversion/CarDelegateMethodGroupConversion/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegateMethodGroupConversion
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        private CarEngineHandler listOfHandlers;
+
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            if (listOfHandlers == null)
+                listOfHandlers = methodToCall;
+            else
+                Delegate.Combine(listOfHandlers, methodToCall);
+        }
+
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers -= methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry, this car is dead...");
+            }
+
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy!  Gonna blow!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CarDelegateMethodGroupConversion/CarDelegateMethodGroupConversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarDelegateMethodGroupConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Method Group Conversion *****\n");
+            Car c1 = new Car();
+
+            c1.RegisterWithCarEngine(CallMeHere);
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            c1.UnRegisterWithCarEngine(CallMeHere);
+
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        static void CallMeHere(string msg)
+        {
+            Console.WriteLine("=> Message from Car: {0}", msg);
+        }
+    }
+    
+}
