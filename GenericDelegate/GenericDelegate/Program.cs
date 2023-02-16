131deb0ae68a30248c39ea8b5fbfe59d22efff41
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-16 23:26:20 +0300

    GenericDelegate
    Delegate with generic

---
+++ GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericDelegate
+{
+    internal class Program
+    {
+        public delegate void MyGenericDelegate<T>(T arg);
+
+        static void Main(string[] args)
+        {
+            MyGenericDelegate<string> strTarget =
+                new MyGenericDelegate<string>(StringTarget);
+
+            strTarget("Some string data");
+
+            MyGenericDelegate<int> intTarget =
+                new MyGenericDelegate<int>(IntTarget);
+            intTarget(9);
+
+            static void StringTarget(string arg)
+            {
+                Console.WriteLine("arg in uppercase is :{0}", arg.ToUpper());
+            }
+
+            static void IntTarget(int arg)
+            {
+                Console.WriteLine("++arg is: {0}", ++arg);
+            }
+        }
+    }
+} 
+    
+
