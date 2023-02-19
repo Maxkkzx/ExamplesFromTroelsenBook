ab9524e2a5da6c1ab288a55328fbb0ddd6a33f34
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-20 00:07:56 +0300

    PublicDelegateProblem
    Problem when declaring public delegates

---
+++ PublicDelegateProblem/PublicDelegateProblem/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PublicDelegateProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Agh! No Encapsulation!*****\n");
+
+            Car myCar = new Car();
+
+            myCar.listOfHandlers = new Car.CarEngineHandler(CallWhenExploded);
+            myCar.Accelerate(10);
+
+            myCar.listOfHandlers = new Car.CarEngineHandler(CallHereToo);
+            myCar.Accelerate(10);
+
+            myCar.listOfHandlers.Invoke("hee, hee, hee...");
+            Console.ReadLine();
+        }
+        static void CallWhenExploded(string msg)
+        { 
+            Console.WriteLine(msg); 
+        }
+        static void CallHereToo(string msg)
+        { 
+            Console.WriteLine(msg); 
+        }
+    }
+
+    public class Car
+    {
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        public CarEngineHandler listOfHandlers;
+
+        public void Accelerate(int delta)
+        {
+            if (listOfHandlers != null)
+                listOfHandlers("Sorry, this car is dead...");
+        }
+    }
+}
+
+
