c162ec69e2870c2960a0f1902efc52c6c668aa89
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-25 23:53:19 +0300

    AnonymousMethod
    Anonymous method

---
+++ AnonymousMethod/AnonymousMethod/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethod
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+
+        public event EventHandler<CarEventsArgs> Exploded;
+        public event EventHandler<CarEventsArgs> AboutToBlow;
+
+        public Car() 
+        { 
+            MaxSpeed = 100; 
+        }
+
+        public Car(string name, int currentSpeed, int maxSpeed)
+        {
+            this.PetName = name;
+            this.CurrentSpeed = currentSpeed;
+            this.MaxSpeed = maxSpeed;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Exploded?.Invoke(this, new CarEventsArgs("Sorry, this car is dead..."));
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (10 == MaxSpeed - CurrentSpeed)
+                {
+                    AboutToBlow(this, new CarEventsArgs("Careful buddy! Gonna blow!"));
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+
+        }
+    }
+}
---
+++ AnonymousMethod/AnonymousMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AnonymousMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            int aboutToBlowCounter = 0;
+
+            c1.AboutToBlow += delegate
+            {
+                aboutToBlowCounter++;
+                Console.WriteLine("Eek! Going too fast!");
+            };
+
+            c1.AboutToBlow += delegate (object sender, CarEventsArgs e)
+            {
+                aboutToBlowCounter++;
+                Console.WriteLine("Message from Car: {0}", e.msg);
+            };
+
+            c1.Exploded += delegate (object sender, CarEventsArgs e)
+            {
+                Console.WriteLine("Fatal message from Car: {0}", e.msg);
+            };
+
+
+            for(int i = 0; i < 6 ; i++)
+            {
+                c1.Accelerate(20);
+            }
+            Console.WriteLine("AboutToBlow event was fired {0} times.", aboutToBlowCounter);
+        }
+    }
+}
