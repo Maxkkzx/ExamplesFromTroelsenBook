0376e0f28ae33df095b9f532a8bd7106c168d1d9
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-27 00:56:05 +0300

    CarEventsWithLambdas
    Class CarEvents with Lambda expression

---
+++ CarEventsWithLambdas/CarEventsWithLambdas/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventsWithLambdas
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public delegate void CarEngineHandler(object sender, CarEventArgs e);
+
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        public Car() { MaxSpeed = 100; }
+
+        public Car(string name, int currentSpeed, int maxSpeed)
+        {
+            this.PetName = name;
+            this.CurrentSpeed = currentSpeed;
+            this.MaxSpeed = maxSpeed;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Exploded?.Invoke(this, new CarEventArgs("Sorry, this car is dead..."));
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (10 == MaxSpeed - CurrentSpeed)
+                {
+                    AboutToBlow?.Invoke(this, new CarEventArgs("Careful buddy!  Gonna blow!"));
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CarEventsWithLambdas/CarEventsWithLambdas/Program.cs
@@ -0,0 +1,27 @@
+﻿using CarEventsWithLambdas;
+
+namespace CarEventsWithLambdas
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Events *****\n");
+
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            c1.AboutToBlow += (sender, e) => { Console.WriteLine(e.msg); };
+            c1.Exploded += (sender, e) => { Console.WriteLine(e.msg); };
+
+            Console.WriteLine("***** Speeding up *****");
+
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.WriteLine("\n***** Speeding up *****");
+
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+        }
+    }
+}
