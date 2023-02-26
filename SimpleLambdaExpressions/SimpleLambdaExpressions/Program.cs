c371445744ca482fac5f42a4c5778b4fbed35879
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-26 19:12:08 +0300

    SimpleLambdaExpressions
    Labmda expression

---
+++ SimpleLambdaExpressions/SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleLambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LabdaExpressionSyntax();
+        }
+
+        static void LabdaExpressionSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            List<int> evenNumbers = list.FindAll((i) =>
+            {
+                Console.WriteLine("value of is currently: {0}", i);
+                bool isEven = ((i % 2) == 0);
+                return isEven;
+            });
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write($"{evenNumber}\t");
+            }
+            Console.WriteLine();    
+        }
+    }
+}
