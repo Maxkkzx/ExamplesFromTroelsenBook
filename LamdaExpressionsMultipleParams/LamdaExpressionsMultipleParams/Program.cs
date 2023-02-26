168343db2573d8821fe6f8e6ba4b66458e5d15dd
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-27 00:40:07 +0300

    LamdaExpressionsMultipleParams
    Lamda expression with two params

---
+++ LamdaExpressionsMultipleParams/LamdaExpressionsMultipleParams/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LamdaExpressionsMultipleParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMath m = new SimpleMath();
+            m.SetMathHandler((msg, result) =>
+            {
+                Console.WriteLine($"Message: {msg}, Result: {result}");
+            });
+
+            m.Add(10, 10);
+
+        }
+    }
+}
---
+++ LamdaExpressionsMultipleParams/LamdaExpressionsMultipleParams/SimpleMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpressionsMultipleParams
+{
+    class SimpleMath
+    {
+        public delegate void MathMessage(string msg, int result);
+        private MathMessage mmDelegate;
+
+        public void SetMathHandler(MathMessage target)
+        {
+            mmDelegate = target;
+        }
+        public void Add(int x, int y)
+        {
+            mmDelegate?.Invoke("Adding has completed!", x + y);
+        }
+ 
+    }
+}
