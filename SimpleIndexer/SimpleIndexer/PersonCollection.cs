d21d6f68dd57f5e7cf3b92f051574b39b136ef36
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-02-27 20:35:04 +0300

    SimpleIndexer
    Simple indexer

---
+++ SimpleIndexer/SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+
+        public Person GetPerson(int pos) => (Person)arPeople[pos];
+        
+        public void AddPerson(Person p) 
+        { 
+            arPeople.Add(p); 
+        }
+
+        public void ClearPeople()
+        {
+            arPeople.Clear();
+        }
+
+        public int Count => arPeople.Count;
+
+        IEnumerator IEnumerable.GetEnumerator() => arPeople.GetEnumerator();
+
+        public Person this[int index]
+        {
+            get => (Person)arPeople[index];
+            set => arPeople.Insert(index, value);
+        }
+    }
+}
---
+++ SimpleIndexer/SimpleIndexer/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SimpleIndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Indexers *****\n");
+
+            PersonCollection myPeople = new PersonCollection();
+
+            myPeople[0] = new Person("Homer", "Simpson", 40);
+            myPeople[1] = new Person("Marge", "Simpson", 38);
+            myPeople[2] = new Person("Lisa", "Simpson", 9);
+            myPeople[3] = new Person("Bart", "Simpson", 7);
+            myPeople[4] = new Person("Maggie", "Simpson", 2);
+
+            for (int i = 0; i < myPeople.Count; i++)
+            {
+                Console.WriteLine($"Person number: {i}");
+                Console.WriteLine($"Name: {myPeople[i].FirstName} {myPeople[i].LastName}");
+                Console.WriteLine($"Age: {myPeople[i].Age}");
+                Console.WriteLine();
+            }
+
+            static void UseGenericListOfPeople()
+            {
+                List<Person> myPeople = new List<Person>();
+
+                myPeople.Add(new Person("Lisa", "Simpson", 9));
+                myPeople.Add(new Person("Bart", "Simpson", 7));
+
+                myPeople[0] = new Person("Maggie", "Simpson", 2);
+
+                for (int i = 0; i < myPeople.Count; i++)
+                {
+                    Console.WriteLine("Person number: {0}", i);
+                    Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName,
+                      myPeople[i].LastName);
+                    Console.WriteLine("Age: {0}", myPeople[i].Age);
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
