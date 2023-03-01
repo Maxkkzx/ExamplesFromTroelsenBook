f49087576f78e6d0de1683d7987e788673e9eb33
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-01 23:37:41 +0300

    OverloadedOps
    Overloading operations

---
+++ OverloadedOps/OverloadedOps/Point.cs
@@ -0,0 +1,66 @@
+﻿using System.Numerics;
+
+namespace OverloadedOps 
+{
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.X},{this.Y}]";
+        }
+
+        public static Point operator + (Point p1, Point p2) =>
+            new Point(p1.X + p2.X , p1.Y + p2.Y);
+
+        public static Point operator - (Point p1, Point p2) =>
+            new Point(p1.X - p2.X, p1.Y - p2.Y);
+
+        public static Point operator + (Point p1, int change) =>
+            new Point(p1.X + change, p1.Y + change);
+        public static Point operator +(int change, Point p1) =>
+            new Point(p1.X + change, p1.Y + change);
+
+        public static Point operator ++(Point p1) => 
+            new Point(p1.X + 1, p1.Y + 1);
+        public static Point operator --(Point p1) => 
+            new Point(p1.X - 1, p1.Y - 1);
+
+        public override bool Equals(object? obj)
+        {
+            return obj.ToString() == this.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public static bool operator == (Point p1, Point p2) => p1.Equals(p2);
+
+        public static bool operator != (Point p1, Point p2) => !p1.Equals(p2);
+
+        public int CompareTo(Point other)
+        {
+            if(this.X > other.X && this.Y > other.Y)
+                return 1;
+            if(this.X < other.X && this.Y < other.Y)
+                return -1;
+            else
+                return 0;
+        }
+
+        public static bool operator < (Point pi, Point p2) => pi.CompareTo(p2) < 0;
+        public static bool operator > (Point pi, Point p2) => pi.CompareTo(p2) > 0;
+        public static bool operator <= (Point pi, Point p2) => pi.CompareTo(p2) <= 0;
+        public static bool operator >= (Point pi, Point p2) => pi.CompareTo(p2) >= 0;
+    }
+}
---
+++ OverloadedOps/OverloadedOps/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace OverloadedOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point ptOne = new Point(100, 100);
+            Point ptTwo = new Point(40, 40);
+
+            Console.WriteLine($"ptOne = {ptOne}");
+            Console.WriteLine($"ptTwo = {ptTwo}");
+
+            Console.WriteLine($"ptOne + ptTwo: {ptOne + ptTwo}");
+            Console.WriteLine($"ptOne - ptTwo: {ptOne - ptTwo}");
+            Console.WriteLine();
+
+            Point biggerPoint = ptOne + 10;
+
+            Console.WriteLine($"ptOne + 10 = {biggerPoint}");
+            Console.WriteLine($"10 + biggerPoint = {10 + biggerPoint}");
+            Console.WriteLine();
+
+            Point ptThree = new Point(90, 5);
+            Console.WriteLine($"ptThree = {ptThree}");
+            Console.WriteLine($"ptThree += ptTwo: {ptThree += ptTwo}");
+            Console.WriteLine();
+
+            Point ptFour = new Point(0, 500);
+            Console.WriteLine($"ptFour = {ptFour}");
+            Console.WriteLine($"ptFour -= ptThree: {ptFour - ptThree}");
+            Console.WriteLine();
+
+            Point ptFive = new Point(1, 1);
+            Console.WriteLine($"++ptFive = {++ptFive}");
+            Console.WriteLine($"--ptFive = {--ptFive}");
+            Console.WriteLine();
+
+            Point ptSix = new Point(20, 20);
+            Console.WriteLine($"ptSix++ = {ptSix++}");
+            Console.WriteLine($"ptSix-- = {ptSix--}");
+            Console.WriteLine();
+
+            Console.WriteLine($"ptOne == ptTwo : {ptOne == ptTwo}");
+            Console.WriteLine($"ptOne != ptTwo : {ptOne != ptTwo}");
+            Console.WriteLine();
+
+            Console.WriteLine("ptOne < ptTwo : {0}", ptOne < ptTwo);
+            Console.WriteLine("ptOne > ptTwo : {0}", ptOne > ptTwo);
+            Console.WriteLine();
+        }
+    }
+}
