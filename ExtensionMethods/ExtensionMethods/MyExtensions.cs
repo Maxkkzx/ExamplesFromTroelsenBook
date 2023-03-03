f42945b85848c6cedd667c0cedb4e01af04ca310
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-03 22:47:18 +0300

    ExtensionMethods
    Extension methods

---
+++ ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensionMethods
+{
+    static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1}\n", obj.GetType().Name, 
+                Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        public static int ReverseDigits(this int i)
+        {
+            char[] digits = i.ToString().ToCharArray();
+
+            Array.Reverse(digits);
+
+            string newDigits = new string(digits);
+
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtensionMethods;
+
+namespace ExtensionMethods
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            Console.WriteLine("Value of myInt: {0}", myInt);
+            Console.WriteLine("Reversed digits of myInt: {0}", myInt.ReverseDigits());
+        }
+    }
+}
