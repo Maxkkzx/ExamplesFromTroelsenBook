9af914e251db800e8c166edbe8a2b5b9304687c2
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-04 21:00:09 +0300

    InterfaceExtensions
    Extension of types that implement specific interfaces

---
+++ InterfaceExtensions/InterfaceExtensions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace InterfaceExtensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = {"Wow", "this", "is", "sort", "of", "annoying",
+            "but", "in", "a", "weird", "way", "fun!",};
+            
+            data.PrintDataAndBeep();
+            Console.WriteLine();
+
+            List<int> myInts = new List<int>() { 10, 15, 20 };
+            myInts.PrintDataAndBeep();
+        }
+    }
+}
