b8931f47dd02c57124bf76178243c40d72511935
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-08 23:17:08 +0300

    LinqRetValues
    Returning LINQ query results

---
+++ LinqRetValues/LinqRetValues/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LinqRetValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> subset = GetStringSubset();
+
+            foreach (string item in subset)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (string item in GetStringSubsetAsArray())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = {"Light Red", "Green",
+                "Yellow", "Dark Red", "Red", "Purple" };
+
+            var theRedColors = from c in colors
+                               where c.Contains("Red")
+                               select c;
+
+            return theRedColors;
+        }
+
+        static IEnumerable<string> GetStringSubsetAsArray()
+        {
+            string[] colors = {"Light Red", "Green",
+                "Yellow", "Dark Red", "Red", "Purple" };
+
+            var theRedColors = from c in colors
+                               where c.Contains("Red")
+                               select c;
+
+            return theRedColors.ToArray();
+        }
+    }
+}
