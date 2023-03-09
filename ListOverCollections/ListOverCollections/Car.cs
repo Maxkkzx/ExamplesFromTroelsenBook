094574d6a7e4664fec06048438fe228342338f44
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-09 21:41:48 +0300

    ListOverCollections
    Applying LINQ queries to Collection objects

---
+++ ListOverCollections/ListOverCollections/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOverCollections
+{
+    internal class Car
+    {
+        public string PetName { get; set; } = "";
+        public string Color { get; set; } = "";
+        public int Speed { get; set; }
+        public string Make { get; set; } = "";
+
+
+    }
+}
---
+++ ListOverCollections/ListOverCollections/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+
+namespace ListOverCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car { PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW" },
+                new Car { PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
+                new Car { PetName = "Mary", Color = "Black", Speed = 55, Make = "VM"},
+                new Car { PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+                new Car { PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"},
+            };
+
+            GetFastCars(cars);
+            GetFastBMWs(cars);
+            LINQOverArrayList();
+            OfTypeAsFilter();
+        }
+
+        static void GetFastCars(List<Car> cars)
+        {
+            var fastCars = from c in cars where c.Speed > 55 select c;
+
+            foreach(var car  in fastCars)
+            {
+                Console.WriteLine($"{car.PetName} is going too fast!");
+            }
+            Console.WriteLine();
+        }
+
+        static void GetFastBMWs(List<Car> cars)
+        {
+            var fastCars =
+                from c in cars where c.Speed > 90 && c.Make == "BMW" select c;
+            
+            foreach(var car in fastCars)
+            {
+                Console.WriteLine($"{car.PetName} is going too fast!!");
+            }
+            Console.WriteLine();
+        }
+
+        static void LINQOverArrayList()
+        {
+            Console.WriteLine("***** LINQ over ArrayList *****");
+
+            ArrayList cars = new ArrayList()
+            {
+                new Car { PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW" },
+                new Car { PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
+                new Car { PetName = "Mary", Color = "Black", Speed = 55, Make = "VM"},
+                new Car { PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+                new Car { PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"},
+            };
+
+            var myCarsEnum = cars.OfType<Car>();
+
+            var fastCars = from c in myCarsEnum where c.Speed > 55 select c;
+
+            foreach(Car car in fastCars)
+            {
+                Console.WriteLine($"{car.PetName} is going too fast!!!");
+            }
+            Console.WriteLine();
+        }
+
+        static void OfTypeAsFilter()
+        {
+            ArrayList myStuff = new ArrayList();
+
+            myStuff.AddRange(new object[] { 10, 400, 8, false, new Car(), "string data" });
+            var myInts = myStuff.OfType<int>();
+
+            foreach(int i in myInts)
+            {
+                Console.WriteLine($"Int values: {i}");
+            }
+        }
+    }
+}
