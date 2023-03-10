e54f7d44a9041b37c85927d764c6579ef7ac2cd1
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-10 21:13:40 +0300

    CheckingToPalindrome
    Palindrome test method

---
+++ CheckingToPalindrome/CheckingToPalindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CheckingToPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputString = Console.ReadLine().ToLower();
+
+            goodWay(inputString);
+        }
+
+        static void goodWay(string inputString)
+        {
+            Console.WriteLine("Good way!");
+
+            string supString = inputString;
+            char[] charArray = supString.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            supString = new string(charArray);
+
+            if (supString == inputString)
+            {
+                Console.WriteLine("It's a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It's not a palindrome");
+            }
+        }
+    }
+}
