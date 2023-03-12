955dd22168be2713a4a5e9fc42102fb5f8b9223a
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-12 20:14:20 +0300

    LinqUsingEnumerable
    Linq using enumerable

---
+++ LinqUsingEnumerable/LinqUsingEnumerable/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LinqUsingEnumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryStringWithOperators();
+            QueryStringsWithEnumerableAndLambdas();
+            QueryStringsWithAnonymousMethods();
+            VeryComplexQueryExpression.QueryStringsWithRawDelegates();
+
+        }
+
+        static void QueryStringWithOperators()
+        {
+            Console.WriteLine("***** Using Query Operators *****");
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            var subset = from game in currentVideoGames where game.Contains(" ") orderby game select game;
+
+            foreach (string s in subset)
+                Console.WriteLine($"Item: {s}");
+            Console.WriteLine();
+        }
+
+        static void QueryStringsWithEnumerableAndLambdas()
+        {
+            Console.WriteLine("***** Using Enumerable / Lambda Expressions *****");
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            var subset = currentVideoGames.Where(game => game.Contains(" ")).OrderBy(game => game).Select(game => game);
+
+            foreach (var game in subset)
+                Console.WriteLine($"Item: {game}");
+            Console.WriteLine();
+        }
+
+        static void QueryStringsWithAnonymousMethods()
+        {
+            Console.WriteLine("***** Using Anonymous Methods *****");
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            Func<string, bool> searchFilter =
+                 delegate (string game) { return game.Contains(" "); };
+
+            Func<string, string> itemToProccess =
+                delegate (string s) { return s; };
+
+            var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProccess).Select(itemToProccess);
+
+            foreach (var game in subset)
+                Console.WriteLine($"Item: {game}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinqUsingEnumerable/LinqUsingEnumerable/VeryComplexQueryExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUsingEnumerable
+{
+    internal class VeryComplexQueryExpression
+    {
+        public static void QueryStringsWithRawDelegates()
+        {
+            Console.WriteLine("***** Using Raw Delegates *****");
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            Func<string, bool> searchFilter = new Func<string, bool>(Filter);
+            Func<string, string> itemToProccess = new Func<string, string>(ProcessItem);
+
+            var subset = currentVideoGames.Where(searchFilter).OrderBy(itemToProccess).Select(itemToProccess);
+
+            foreach (var game in subset)
+                Console.WriteLine($"Item: {game}");
+            Console.WriteLine();
+        }
+            public static bool Filter(string game) { return game.Contains(" "); }
+            public static string ProcessItem(string game) { return game; }
+    }
+}
