9f31a92846890686e1d1b0a85c8be574cd4019b8
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-20 22:59:05 +0300

    SimpleGC
    Garbage collection

---
+++ SimpleGC/SimpleGC/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    internal class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        public Car() { }
+        
+        public Car(string petName, int currentSpeed)
+        {
+            CurrentSpeed = currentSpeed;
+            PetName = petName;
+        }
+
+        public override string ToString() => $"{PetName} is going {CurrentSpeed} MPH";
+    }
+}
---
+++ SimpleGC/SimpleGC/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SimpleGC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** GC Basics *****");
+
+            MakeACar();
+
+            Console.WriteLine($"Enstimated byte on heap : {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"This OS has {GC.MaxGeneration + 1} object generations.\n");
+
+            Car refToMyCar = new Car("Zippy", 100);
+            Console.WriteLine(refToMyCar.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine($"Generation of  refToMyCar is {GC.GetGeneration(refToMyCar)}");
+
+            object[] tonsOfObjects = new object[50000];
+            for (int i = 0; i < 50000; i++)
+                tonsOfObjects[i] = new object();
+
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine($"Generation of refToMyCar is: {GC.GetGeneration(refToMyCar)}");
+
+            if (tonsOfObjects[9000] != null)
+            {
+                Console.WriteLine($"Generation of tonsOfObjects[9000] is: {GC.GetGeneration(tonsOfObjects[9000])}");
+            }
+            else
+                Console.WriteLine("tonsOfObject[9000] is no longer alive");
+
+            Console.WriteLine("\nGen 0 has been swept {0} times",
+                GC.CollectionCount(0));
+
+            Console.WriteLine("Gen 1 has been swept {0} times",
+                GC.CollectionCount(1));
+
+            Console.WriteLine("Gen 2 has been swept {0} times",
+                GC.CollectionCount(2));
+
+        }
+
+        static void MakeACar()
+        {
+            Car myCar = new Car();
+            myCar = null;
+        }
+    }
+}
