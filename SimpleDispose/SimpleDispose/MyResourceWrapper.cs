1a57afa6f7da49c0413043daa2f1ff2239a3913c
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-21 18:57:44 +0300

    SimpleDispose
    Dispose method

---
+++ SimpleDispose/SimpleDispose/MyResourceWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    internal class MyResourceWrappe : IDisposable
+    {
+        public void Dispose() 
+        {
+            Console.WriteLine("***** In Dispose! *****");
+        }  
+    }
+}
---
+++ SimpleDispose/SimpleDispose/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleDispose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Dispose *****\n");
+
+            using (MyResourceWrappe rw = new MyResourceWrappe(),
+                rw2 = new MyResourceWrappe())
+            {
+                //
+            }
+        }
+    }
+}
