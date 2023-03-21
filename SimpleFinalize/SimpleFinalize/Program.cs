998606a7eb1e2237747b85b2bf771750ec616dc6
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-21 17:50:49 +0300

    SimpleFinalize
    Overriding the finalize method

---
+++ SimpleFinalize/SimpleFinalize/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleFinalize
+{
+    internal class Program
+    {
+        class MyResourceWrapper
+        {
+            ~MyResourceWrapper() => Console.Beep();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Finalizers *****\n");
+            Console.WriteLine("Hit the return key to shut down this app");
+            Console.WriteLine("and force the GC to invoke FinalizeO");
+            Console.WriteLine("for finalizable objects created in this AppDomain.");
+            Console.ReadLine();
+
+            MyResourceWrapper resourceWrapper = new MyResourceWrapper();
+        }
+    }
+}
