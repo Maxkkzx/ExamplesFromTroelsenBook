a55dae5109de4af53f74c1805af173fe15e32e62
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-22 21:05:53 +0300

    FinalizeDisposableClass
    Final version with dispose and finalize

---
+++ FinalizableDisposableClass/FinalizableDisposableClass/MyResourceWrapper.cs
@@ -0,0 +1,32 @@
+﻿namespace FinalizableDisposableClass
+{
+    internal class MyResourceWrapper : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            CleanUp(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+
+                }
+            }
+            disposed = true;
+        }
+        ~MyResourceWrapper()
+        {
+            Console.Beep();
+
+            CleanUp(false);
+        }
+    }
+}
---
+++ FinalizableDisposableClass/FinalizableDisposableClass/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalizableDisposableClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter *****"); 
+
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose();
+
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+
+            Console.ReadLine();
+        }
+    }
+}
