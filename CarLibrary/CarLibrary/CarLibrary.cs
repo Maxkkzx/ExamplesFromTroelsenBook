3ddc6c738ee92439c5061726e8db1aa5f451d667
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-03-28 23:52:18 +0300

    CarLibrary
    Building and consuming a special class library

---
+++ CarLibrary/CarLibrary/CarLibrary.cs
@@ -0,0 +1,23 @@
+﻿namespace CarLibrary
+{
+    public enum EngineState
+    { engineAlive, engineDead}
+
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState => egnState;
+        public abstract void TurboBoost();
+        public Car() { }
+        public Car(string petName,  int maxSpeed, int currentSpeed)
+        {
+            PetName = petName;
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+        }
+
+    }
+}
---
+++ CarLibrary/CarLibrary/DerivedCars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    internal class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSp, int curSp)
+            : base(name, maxSp, curSp) { }
+
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Ramming speed!", "Faster is better...");
+        }
+    }
+
+    internal class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSp, int curSp)
+            : base (name, maxSp, curSp) { }
+
+        public override void TurboBoost()
+        {
+            egnState = EngineState.engineDead;
+            MessageBox.Show("Eek!", "Your engine block exploded!");
+        }
+    }
+}
