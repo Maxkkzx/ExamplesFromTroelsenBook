69883da3cbbf0c07e5854f9728361b8ac11a3b6d
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-06-01 19:50:07 +0300

    AttributedCarLibrary
    Building special attributes

---
+++ AttributedCarLibrary/AttributedCarLibrary/Types.cs
@@ -0,0 +1,34 @@
+﻿[assembly: CLSCompliant(true)]
+namespace AttributedCarLibrary
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    internal sealed class Types : System.Attribute
+    {
+        public string Description { get; set; }
+        internal Types(string vehicleDescription)
+            => Description = vehicleDescription;
+        internal Types() { }
+    }
+
+    [Serializable]
+    [Types(Description = "My rocking Harley!")]
+    internal class Motorcycle
+    {
+    }
+
+    [Serializable]
+    [Obsolete("Use another vehicle!")]
+    [Types("The old gray mare, she ain't what she used to be...")]
+    internal class HorseAndBuggy
+    {
+    }
+
+    [Types("A very long, slow, but feature-rich auto")]
+    internal class Winnebago
+    {
+        public void PlayMusic(bool On)
+        {
+
+        }
+    }
+}
