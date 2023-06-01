bff6f81c8c76f48e8e1123a652e151ecc7574784
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-06-01 20:06:06 +0300

    VehicleDescriptionAttributeReader
    Attribute reflection using early binding

---
+++ VehicleDescriptionAttributeReader/VehicleDescriptionAttributeReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using AttributedCarLibrary;
+
+namespace VehicleDescriptionAttributeReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectOnAttnbutesUsingEarlyBinding();
+            Console.ReadLine();
+        }
+
+        private static void ReflectOnAttnbutesUsingEarlyBinding()
+        {
+            Type t = typeof(Winnebago);
+
+            object[] customAtts = t.GetCustomAttributes(false);
+
+            foreach (Types v in customAtts)
+                Console.WriteLine("-> \n", v.Description);
+        }
+    }
+}
