46cf3260a4295bfb093177ada199c6abc1a612d1
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-06-22 21:28:58 +0300

    LateBindingWithDynamic
    Using the dynamic keyword to pass arguments

---
+++ LateBindingWithDynamic/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace LateBindingWithDynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddWithReflection();
+        }
+
+        private static void AddWithReflection()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+
+            try
+            {
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+
+                dynamic obj = Activator.CreateInstance(math);
+
+                Console.WriteLine("Result is: {0}", obj.Add(10,70));
+            }
+
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
