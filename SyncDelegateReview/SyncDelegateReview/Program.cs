4cc037b7d8b26f6f2550bfb9fbfce2ae0dce18c4
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-10-31 21:55:11 +0300

    SyncDelegateReview
    Static method Add () is called in synchronous mode

---
+++ SyncDelegateReview/SyncDelegateReview/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SyncDelegateReview
+{
+    public delegate  int BinaryOp(int x,  int y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Synch Delegate Review *****");
+
+            Console.WriteLine("Main() invoked on thread {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp b = new BinaryOp(Add);
+
+            int answer = b(10, 10);
+
+            Console.WriteLine("Doing more work in Main()");
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine("Add() invoked on thread {0}",
+                Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
