35faade7532fb2fefd7723bae8a7e7d3e324f4ca
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-11-01 19:01:21 +0300

    AsyncDelegate
    Asynchronous method call

---
+++ AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+
+            Console.WriteLine("Main() invoked on thread {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp b = new BinaryOp(Add);
+
+            IAsyncResult ar = b.BeginInvoke(10, 10, null, null);
+
+            while (!ar.AsyncWaitHandle.WaitOne(1000, true))
+            {
+                Console.WriteLine("Doing more work in Main()!");
+            }
+
+            int answer = b.EndInvoke(ar);
+
+            Console.WriteLine("10 + 10 is {0}.", answer);
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine("Add() invoked on thread {0}",
+                Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
