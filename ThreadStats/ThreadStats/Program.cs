7eafa94fca0990835d1c5581582c26285eb1a682
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-11-02 21:53:36 +0300

    ThreadStats
    Getting statistics about the current execution thread

---
+++ ThreadStats/ThreadStats/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadStats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Primary Thread stats *****\n");
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThrePrimaryThread";
+
+            Console.WriteLine($"Name of current AppDomain: {Thread.GetDomain().FriendlyName}");
+            Console.WriteLine($"Id of current AppDomain: {Thread.GetDomainID}");
+
+
+            Console.WriteLine($"Thread Name: {primaryThread.Name}");
+            Console.WriteLine($"Has thread started?: {primaryThread.IsAlive}");
+            Console.WriteLine($"Priority Level: {primaryThread.Priority}");
+            Console.WriteLine($"Thread State: {primaryThread.ThreadState}");
+
+            Console.ReadLine();
+        }
+    }
+}
