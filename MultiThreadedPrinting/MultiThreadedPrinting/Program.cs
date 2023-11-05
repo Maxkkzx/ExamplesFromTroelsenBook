49f79e51e4d932e9bbf772285bdabf4ae992429b
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-11-05 20:28:53 +0300

    MultiThreadedPrinting
    Synchronization using C# lock keyword

---
+++ MultiThreadedPrinting/MultiThreadedPrinting/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MultiThreadPrinting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Synchronizing Threads *****\n");
+
+            Printer p = new Printer();
+
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread #{i}"
+                };
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+
+        public class Printer
+        {
+            private object threadLock = new object();
+            public void PrintNumbers()
+            {
+                lock (threadLock)
+                {
+
+                    Console.WriteLine("-> {0} is executing PrintNumbers ()",
+                        Thread.CurrentThread.Name);
+                    Console.Write("Your numbers: ");
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Random r = new Random();
+                        Thread.Sleep(100 * r.Next(5));
+                        Console.Write($"{i}, ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
