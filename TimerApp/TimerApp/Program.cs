62eec6d70b4d34fcaf8257d997c3461ce18e71e7
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-11-06 19:37:22 +0300

    TimerApp
    Programming with TimerCallBack

---
+++ TimerApp/TimerApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TimerApp
+{
+    internal class Program
+    {
+
+        static void PrintTime(object state)
+        {
+            Console.WriteLine($"Time is {DateTime.Now.ToLongTimeString()}, Param is: {state.ToString()}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Working with Timer type *****\n");
+
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+            var _ = new Timer(
+                timeCB,
+                "Use of autonomous discarding",
+                0,
+                1000);
+            Console.WriteLine("Hit key to  terminate...");
+            Console.ReadLine();
+        }
+    }
+}
