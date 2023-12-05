2660b748b6c169ea2cbf51c3802db6a776405542
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-12-05 20:18:22 +0300

    DriveInfoApp
    Working with Drivelnfoo type

---
+++ DriveInfoApp/DriveInfoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DriveInfoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with DriveInfo *****\n");
+
+            DriveInfo[] myDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in myDrives)
+            {
+                Console.WriteLine($"Name: {d.Name}");
+                Console.WriteLine($"Type: {d.DriveType}");
+
+                if(d.IsReady)
+                {
+                    Console.WriteLine($"Free Space: {d.TotalFreeSpace}");   
+                    Console.WriteLine($"Format: {d.DriveFormat}");
+                    Console.WriteLine($"Label: {d.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
