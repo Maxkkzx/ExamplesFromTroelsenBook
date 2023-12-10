9bfc02c365429d8550a31c95c6bbf7e2ad9afa4d
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-12-10 23:06:58 +0300

    SimpleFileIO
    Working with File type

---
+++ SimpleFilelO/SimpleFilelO/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleFilelO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple I/O with the File Type *****");
+            string[] myTasks =
+            {
+                "Fix bathroom sink", "Call Dave",
+                "Call Mom and Dad", "Play Xbox One"
+            };
+
+            File.WriteAllLines(@"tasks.txt", myTasks);
+            
+            foreach (string task in File.ReadAllLines(@"tasks.txt"))
+            {
+                Console.WriteLine("TODO: {0}", task);
+            }
+            Console.ReadLine();
+        } 
+    }
+}
