95aff813bbb716fe11abae92fb982f132aacf50e
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-12-11 21:06:26 +0300

    StreamWriterReaderApp
    Working with StreamWriter and StreamReader

---
+++ StreamWriterReaderApp/StreamWriterReaderApp/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace StreamWriterReaderApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with StreamWriter / StreamReader *****\n");
+
+            using (StreamWriter writer = File.CreateText("reminders.txt"))
+            {
+                writer.WriteLine("Don't forget Mother's Day this year...");
+                writer.WriteLine("Don't forget Mother's Day this year...");
+                writer.WriteLine("Don't forget these numbers:");
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.Write(i + " ");
+                }
+                writer.Write(writer.NewLine);
+            }
+            Console.WriteLine("Created file and wrote some thoughts...");
+            Console.ReadLine();
+
+            Console.WriteLine("Here are you thoughts:\n");
+
+            using (StreamReader sr = File.OpenText("reminders.txt"))
+            {
+                string input = null;
+
+                while ((input = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter("reminders.txt"))
+            {
+                writer.WriteLine("Don't forget Mother's Day this year...");
+                writer.WriteLine("Don't forget Mother's Day this year...");
+                writer.WriteLine("Don't forget these numbers:");
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.Write(i + " ");
+                }
+                writer.Write(writer.NewLine);
+            }
+            Console.WriteLine("Created file and wrote some thoughts...");
+            Console.ReadLine();
+
+            Console.WriteLine("Here are you thoughts:\n");
+
+            using (StreamReader sr = new StreamReader("reminders.txt"))
+            {
+                string input = null;
+
+                while ((input = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
