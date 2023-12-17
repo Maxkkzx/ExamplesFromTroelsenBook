a22275e5d10277ee52433212d617e47abf661cf7
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2023-12-17 19:16:36 +0300

    MyDirectoryWatcher
    Software file tracking

---
+++ MyDirectoryWatcher/MyDirectoryWatcher/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MyDirectoryWatcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** The Amazing File Watcher App *****\n");
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            try
+            {
+                watcher.Path = @"C:\MyFolder";
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+            | NotifyFilters.LastWrite
+            | NotifyFilters.FileName
+            | NotifyFilters.DirectoryName;
+
+            watcher.Filter = "*.txt";
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine(@"Press 'q' to quit app.");
+            while (Console.Read() != 'q')
+                ;
+        }
+
+        static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
+        }
+
+        static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
