bcf547cedeade1e2ca03c8afb1dc5fed3f94f3d6
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2024-01-05 21:42:16 +0300

    MyConnectionFactory
    Abstraction of data providers using interfaces

---
+++ MyConnectionFactory/MyConnectionFactory/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Net.Http.Headers;
+using System.Configuration;
+
+namespace MyConnectionFactory
+{
+    enum DataProvider
+    {
+        SqlServer, OleDb, Odbc, None
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Very Simple Connection Factory *****\n");
+
+            string dataProviderString = ConfigurationManager.AppSettings["provider"];
+
+            DataProvider dataProvider = DataProvider.None;
+
+            if(Enum.IsDefined(typeof(DataProvider), dataProviderString))
+            {
+                dataProvider =
+                    (DataProvider)Enum.Parse(typeof(DataProvider),dataProviderString);
+            }
+            else
+            {
+                WriteLine("Sorry, no provider exists!");
+                ReadLine();
+                return;
+            }
+
+            IDbConnection myConnection = GetConnection(dataProvider);
+
+            WriteLine($"Your connection is a {myConnection.GetType().Name ?? "unrecognized type"}");
+            ReadLine();
+        }
+
+        static IDbConnection GetConnection(DataProvider dataProvider) 
+        {
+            IDbConnection connection = null;
+            switch (dataProvider)
+            {
+                case DataProvider.SqlServer:
+                    connection = new SqlConnection();
+                    break;
+                case DataProvider.OleDb:
+                    connection = new OleDbConnection();
+                    break;
+                case DataProvider.Odbc:
+                    connection = new OdbcConnection();
+                    break;
+            }
+            return connection;
+        }
+
+
+    }
+}
