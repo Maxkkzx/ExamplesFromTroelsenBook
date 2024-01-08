ab322d1cc29007436aee34bf56fd1c792feff93e
Maxim <105167571+Maxkkzx@users.noreply.github.com>
2024-01-08 22:39:00 +0300

    AutoLotDataReader
    The concept of the ADO.NET connected layer

---
+++ AutoLotDataReader/AutoLotDataReader/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace AutoLotDataReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("***** Fun with Data Readers *****\n");
+
+            var cnStringBuilder = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = "AutoLot",
+                DataSource = "DESKTOP-HFDL99A",
+                ConnectTimeout = 30,
+                IntegratedSecurity = true
+
+            };
+
+            using(var connection = new SqlConnection())
+            {
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+
+                ShowConnectionStatus(connection);
+
+                string sql = "Select * From Inventory;Select * from Customers";
+
+                using (SqlCommand myCommand = new SqlCommand(sql, connection))
+                {
+
+                    using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                    {
+                        do
+                        {
+                            while (myDataReader.Read())
+                            {
+                                WriteLine("***** Record *****");
+
+                                for (int i = 0; i < myDataReader.FieldCount; i++)
+                                {
+                                    WriteLine($"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)}");
+                                }
+
+                                WriteLine();
+                            }
+                        } while (myDataReader.NextResult());
+                    }
+                }
+            }
+            ReadLine();
+        }
+
+        static void ShowConnectionStatus(SqlConnection connection)
+        {
+            WriteLine("***** Info about your connection *****");
+
+            WriteLine($"Database location: {connection.DataSource}");
+            WriteLine($"Database name: {connection.Database}");
+            WriteLine($"Timeout: {connection.ConnectionTimeout}");
+            WriteLine($"Connection state: {connection.State}\n");
+
+        }
+    }
+}
